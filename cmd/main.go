8f7cee7518d8e65ea5af3d9ce9f89b31cd990518
marsin134 <lampetow555@gmail.com>
2026-02-04 00:03:08 +0300

    added config

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"dailyPlanner/internal/config"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Uploading .env
+	if err := config.LoadEnvFile(".env"); err != nil {
+		log.Printf("Не удалось загрузить .env: %v", err)
+	}
+
+	cfg := config.LoadConfig()
+
+	if cfg.Token.JWTSecret == "" {
+		log.Fatal("JWT_SECRET_KEY не установлен в .env файле")
+	}
+
+	// Starting the server
+	addr := fmt.Sprintf(":%d", cfg.ServerPort)
+	fmt.Printf("Сервер запущен на %s\n", addr)
+
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(addr, nil)
+}
+
+func helloWorld(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
