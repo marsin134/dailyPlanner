4b92df714d3b3cd8c4a6431b1da5693f2f44801d
marsin134 <lampetow555@gmail.com>
2026-02-19 18:58:08 +0300

    added middleware

--- cmd/app/server.go
+++ cmd/app/server.go
@@ -3,6 +3,7 @@ package app
 import (
 	"dailyPlanner/internal/config"
 	"dailyPlanner/internal/handler"
+	"dailyPlanner/internal/middleware"
 	"dailyPlanner/internal/repository"
 	"dailyPlanner/internal/service"
 	"fmt"
@@ -14,7 +15,7 @@ func InitializationHandlers(repo *repository.Repository, svc *service.Service, c
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", helloWorld)
+	mux.HandleFunc("/", HomeHandler)
 
 	mux.HandleFunc("/api/auth/register", handlers.Register)
 	mux.HandleFunc("/api/auth/login", handlers.LoginHandler)
@@ -25,7 +26,7 @@ func InitializationHandlers(repo *repository.Repository, svc *service.Service, c
 	mux.HandleFunc("/api/me", handlers.GetMe)
 	mux.HandleFunc("/api/me/update-name", handlers.UpdateUserNameHandler)
 	mux.HandleFunc("/api/me/update-password", handlers.UpdateUserPasswordHandler)
-	mux.HandleFunc("/api/me/delete", handlers.DeleteUser)
+	mux.HandleFunc("/api/me/delete/", handlers.DeleteEventByIDHandler)
 
 	mux.HandleFunc("/api/user/", handlers.GetByUserIDHandler)
 	mux.HandleFunc("/api/user/appointment-moderator/", handlers.AppointmentModeratorHandler)
@@ -37,9 +38,36 @@ func InitializationHandlers(repo *repository.Repository, svc *service.Service, c
 	mux.HandleFunc("/api/event/update", handlers.UpdateEventHandler)
 	mux.HandleFunc("api/event/delete/", handlers.DeleteEventByIDHandler)
 
-	return mux
+	handlerChain := middleware.Chain(
+		mux,
+		middleware.CORSMiddleware,
+		middleware.AuthMiddleware(cfg),
+		middleware.LoggingMiddleware)
+
+	return handlerChain
 }
 
-func helloWorld(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+func HomeHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "/, HomeHandler\n\n")
+
+	fmt.Fprintf(w, "/api/auth/register, handlers.Register\n")
+	fmt.Fprintf(w, "/api/auth/login, handlers.LoginHandler\n")
+	fmt.Fprintf(w, "/api/auth/refresh-token, handlers.RefreshToken\n")
+	fmt.Fprintf(w, "/api/auth/logout, handlers.LogoutHandler\n")
+	fmt.Fprintf(w, "/api/auth/logout-all, handlers.LogoutAllExceptHandler\n\n")
+
+	fmt.Fprintf(w, "/api/me, handlers.GetMe\n")
+	fmt.Fprintf(w, "/api/me/update-name, handlers.UpdateUserNameHandler\n")
+	fmt.Fprintf(w, "/api/me/update-password, handlers.UpdateUserPasswordHandler\n")
+	fmt.Fprintf(w, "/api/me/delete/, handlers.DeleteEventByIDHandler\n\n")
+
+	fmt.Fprintf(w, "/api/user/, handlers.GetByUserIDHandler\n")
+	fmt.Fprintf(w, "/api/user/appointment-moderator/, handlers.AppointmentModeratorHandler\n\n")
+
+	fmt.Fprintf(w, "/api/event/create, handlers.CreateEventHandler\n")
+	fmt.Fprintf(w, "/api/event/get/, handlers.GetEventByIDHandler\n")
+	fmt.Fprintf(w, "/api/event/get, handlers.GetEventByUserAndDate\n")
+	fmt.Fprintf(w, "/api/event/complete/, handlers.CompleteEvent\n")
+	fmt.Fprintf(w, "/api/event/update, handlers.UpdateEventHandler\n")
+	fmt.Fprintf(w, "api/event/delete/, handlers.DeleteEventByIDHandler\n")
 }
